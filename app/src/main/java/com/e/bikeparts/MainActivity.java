52970fa1a6e006f2385926f656f11edf65919d6b
aniket <aniket@gmail.com>
2019-12-05 10:58:34 +0545

    login and signup form

--- app/src/main/java/com/e/bikeparts/MainActivity.java
+++ app/src/main/java/com/e/bikeparts/MainActivity.java
@@ -2,7 +2,9 @@ package com.e.bikeparts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Login Form");
+
+
+    }
+
+    public void btn_signupForm(View view){
+
+        startActivity(new Intent(getApplicationContext(),Signup_Form.class));
+
     }
 }
