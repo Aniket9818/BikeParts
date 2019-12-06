0f15db3011179a95a71961ea6963ff1acfbe77b7
aniket <aniket@gmail.com>
2019-12-06 12:28:47 +0545

    form and design

--- app/src/main/java/com/e/bikeparts/Signup_Form.java
+++ app/src/main/java/com/e/bikeparts/Signup_Form.java
@@ -2,7 +2,9 @@ package com.e.bikeparts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup_Form extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Signup_Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("SignUp Form");
+
+    }
+
+
+
+    public void btn_mainactivityForm(View view){
+
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
     }
 }
